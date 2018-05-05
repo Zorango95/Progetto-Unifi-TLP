8d7c835a324f9d8267abda31973aebd87ac17c85
Alex Foglia <alex.f1793@gmail.com>
2018-05-05 23:49:09 +0200

    ti ho cagato un combinatore di punto fisso

---
+++ Lamda Expressions/src/Esempi/Factorial.java
@@ -0,0 +1,22 @@
+import java.util.function.Function;
+
+interface SelfApplicable<T> {
+
+	public T apply(SelfApplicable<T> a);
+
+}
+
+public class Factorial {
+
+	public int getFactorial(int n) {
+		
+		SelfApplicable<Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>>> Y = y -> f -> x -> f
+				.apply(y.apply(y).apply(f)).apply(x);
+		
+		
+		Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>> Fix = Y.apply(Y);
+		Function<Function<Integer, Integer>, Function<Integer, Integer>> F = factorial -> x -> x == 0 ? 1 : x * factorial.apply(x - 1);
+	    Function<Integer, Integer> fixfactorial = Fix.apply(F);
+		return fixfactorial.apply(n);
+	}
+}
---
+++ Lamda Expressions/src/Esempi/FactorialTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FactorialTest {
+
+	private Factorial fact = new Factorial();
+	@Test
+	public void testZero(){
+		assertEquals(1,fact.getFactorial(0));
+	}
+	@Test
+	public void test() {
+		for(int i=1; i<10;i++){
+			assertEquals(i*fact.getFactorial(i-1),fact.getFactorial(i));
+		}
+	}
+
+}
