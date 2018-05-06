19f2efb40241675c42b08f0d2769b7b6f152633c
Zorango95 <freewilly1995@gmail.com>
2018-05-06 02:44:01 +0200

    nothing

--- Lamda Expressions/src/Esempi/IntFunProd.java
+++
@@ -1,6 +0,0 @@
-package Esempi;
-
-public interface IntFunProd {
-
-	int operation(int x, int y);
-}
--- Lamda Expressions/src/Esempi/Pattern/Strategy.java
+++
@@ -1,5 +0,0 @@
-package Esempi.Pattern;
-
-public interface Strategy {
-	public double getPrice(double base);
-}
