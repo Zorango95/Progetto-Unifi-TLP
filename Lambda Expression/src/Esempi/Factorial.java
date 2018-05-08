5c76661fa93b45727074b2248647129fc231db42
Alex Foglia <alex.f1793@gmail.com>
2018-05-08 17:58:19 +0200

    tipaggio iniziato

--- Lambda Expression/src/Esempi/Factorial.java
+++ Lambda Expression/src/Esempi/Factorial.java
@@ -12,14 +12,16 @@ public class Factorial {
 
 	public int getFactorial(int n) {
 
-		SelfApplicable<Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>>> Y = y -> f -> x -> f
+		SelfApplicable<Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>>> 
+		Y = y -> f -> x -> f
 				.apply(y.apply(y).apply(f)).apply(x);
-
-		Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>> Fix = Y
-				.apply(Y);
-		Function<Function<Integer, Integer>, Function<Integer, Integer>> F = factorial -> x -> x == 0 ? 1
+		Function<Function<Function<Integer, Integer>, Function<Integer, Integer>>, Function<Integer, Integer>> 
+		Fix = Y.apply(Y);
+		Function<Function<Integer, Integer>, Function<Integer, Integer>> Fact = factorial -> x -> x == 0 ? 1
 				: x * factorial.apply(x - 1);
-		Function<Integer, Integer> fixfactorial = Fix.apply(F);
+		Function<Integer, Integer> 
+		
+		fixfactorial = Fix.apply(Fact);
 		return fixfactorial.apply(n);
 	}
 }
--- Lambda Expression/src/Esempi/Pattern/StrategyDiscount.java
+++ Lambda Expression/src/Esempi/Pattern/StrategyDiscount.java
@@ -1,5 +1,6 @@
 package Esempi.Pattern;
 
+import java.awt.Point;
 import java.util.function.Function;
 
 public class StrategyDiscount implements Strategy {
@@ -7,6 +8,7 @@ public class StrategyDiscount implements Strategy {
 	
 	@Override
 	public double getPrice(double base) {
+		Function<Integer,Integer> a = (x)->(x+1);
 		return 0.75*base;
 	}
 	
