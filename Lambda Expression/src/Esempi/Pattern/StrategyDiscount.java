edc0f512b69910242711fd1d84455095343ba8e3
Alex Foglia <alex.f1793@gmail.com>
2018-05-08 14:17:50 +0200

    presentazione aggiornata

--- Lambda Expression/src/Esempi/Pattern/StrategyDiscount.java
+++ Lambda Expression/src/Esempi/Pattern/StrategyDiscount.java
@@ -1,10 +1,14 @@
 package Esempi.Pattern;
 
+import java.util.function.Function;
+
 public class StrategyDiscount implements Strategy {
+
 	
 	@Override
 	public double getPrice(double base) {
 		return 0.75*base;
 	}
+	
 
 }
